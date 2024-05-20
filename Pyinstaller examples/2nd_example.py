b1f609445cbbc72893a5c1d4e711bb8f1fc1cf0b
MartinAonsoLaland <martin.lalanda@mercedes-benz.com>
2024-05-20 08:10:10 -0500

    Create 2nd_example.py

---
+++ Pyinstaller examples/2nd_example.py
@@ -0,0 +1,24 @@
+# This is an example for a script with s library and no GUI for testing purposes
+# Try pyinstaller on this example
+
+import numpy as np
+def main():
+
+    a = int(input("a = "))
+    b = int(input("b = "))
+    c = int(input("c = "))
+
+    arr1 = np.array([a, b, c])
+    arr2 = np.array([2, 5, 7])
+
+    result = arr1 + arr2
+
+    print("Result of addition:", result)
+
+    result = arr1 * arr2
+
+    print("Result of multiplication:", result)
+
+
+if __name__ == "__main__":
+    main()
